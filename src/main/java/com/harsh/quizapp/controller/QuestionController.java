fe03668cd12cc2e5ea0e0f75f2e60c9907346cf9
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-18 18:24:26 +0530

    Response Entity added

--- src/main/java/com/harsh/quizapp/controller/QuestionController.java
+++ src/main/java/com/harsh/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.harsh.quizapp.controller;
 import com.harsh.quizapp.model.Questions;
 import com.harsh.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,18 +15,18 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Questions> getAllQuestions() {
+    public ResponseEntity<List<Questions>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Questions> getQuestionsByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Questions question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Questions question) {
         return questionService.addQuestion(question);
     }
 }
--- src/main/java/com/harsh/quizapp/service/QuestionService.java
+++ src/main/java/com/harsh/quizapp/service/QuestionService.java
@@ -2,10 +2,14 @@ package com.harsh.quizapp.service;
 
 import com.harsh.quizapp.dao.QuestionDao;
 import com.harsh.quizapp.model.Questions;
+import org.hibernate.jdbc.Expectation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,13 +17,24 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Questions> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Questions>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Questions> getQuestionsByCategory(String category) {
-        String modifiedCategory = capitalizeFirstLetter(category);
-        return questionDao.findByCategory(modifiedCategory); // java -> Java
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(String category) {
+        try {
+            String modifiedCategory = capitalizeFirstLetter(category);
+            return new ResponseEntity<>(questionDao.findByCategory(modifiedCategory), HttpStatus.OK); // java -> Java
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
     private String capitalizeFirstLetter(String str) {
         if (str == null || str.isEmpty()) return str;
@@ -27,9 +42,14 @@ public class QuestionService {
     }
 
 
-    public String addQuestion(Questions question) {
-        questionDao.save(question);
-        return "Success";
+    public ResponseEntity<String> addQuestion(Questions question) {
+        try {
+            questionDao.save(question);
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Failed to add the product", HttpStatus.BAD_REQUEST);
     }
 }
 
