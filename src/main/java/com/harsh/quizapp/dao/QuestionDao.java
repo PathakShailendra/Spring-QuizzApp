3e00d9cbd09ac1bfb4a9166502fbc24c9bdbd520
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-20 17:12:22 +0530

    CreateQuiz controller added

--- src/main/java/com/harsh/quizapp/dao/QuestionDao.java
+++ src/main/java/com/harsh/quizapp/dao/QuestionDao.java
@@ -3,6 +3,7 @@ package com.harsh.quizapp.dao;
 import com.harsh.quizapp.model.Questions;
 import com.harsh.quizapp.model.Questions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface QuestionDao extends JpaRepository<Questions, Integer> {
 
     List<Questions> findByCategory(String category);
 
+    @Query(value = "SELECT * FROM questions AS q WHERE q.category = :category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Questions> findRandomQuestionsByCategory(String category, int numQ);
 }
